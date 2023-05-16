0e82f6576c78f784a2211c4b7a56339046edd1ed
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-17 01:46:05 +0600

    module 70 done

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const CheckOut = () => {
     const service = useLoaderData();
-    const { _id, title, price } = service;
+    const { _id, title, price, img } = service;
+    const {user} = useContext(AuthContext);
+
+    const handleBookService = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = user?.email;
+        const booking = {
+            customerName: name,
+            email,
+            date,
+            img,
+            service: title,
+            service_id: _id,
+            price: price
+        }
+        console.log(booking);
+
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('service book successfully');
+            }
+        })
+    }
+
     return (
         <div>
-            <h2>Book Service: {title}</h2>
-            <form>
+            <h2 className='text-center text-3xl'>Book Service: {title}</h2>
+            <form onSubmit={handleBookService}>
                 <div className="card-body">
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                         <div className="form-control">
                             <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder='name' defaultValue={user?.displayName} className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Date</span>
+                            </label>
+                            <input type="date" name='date' className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" defaultValue={user?.email} className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Due amount</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" defaultValue={'$ '+ price} className="input input-bordered" />
                         </div>
                     </div>
                     <div className="form-control mt-6">
