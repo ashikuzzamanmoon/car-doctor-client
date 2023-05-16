479174545652b1b714477c4b101dfa19fbea55e7
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-16 15:20:03 +0600

    checkout component

---
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const CheckOut = () => {
+    const service = useLoaderData();
+    const { _id, title, price } = service;
+    return (
+        <div>
+            <h2>Book Service: {title}</h2>
+            <form>
+                <div className="card-body">
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                        </div>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-primary btn-block" type="submit" value="Order Confirm" />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default CheckOut;
